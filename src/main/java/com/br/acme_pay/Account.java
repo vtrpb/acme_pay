2b94df0274a4e96cb13dcd2ac87fa7231ec4ce48
vtrpb <vtrpb@hotmail.com>
2024-04-27 09:58:29 -0300

    Sincronizando...

--- src/main/java/com/br/acme_pay/Account.java
+++ src/main/java/com/br/acme_pay/Account.java
@@ -1,9 +1,43 @@
 package br.com.acme_pay;
 
+import java.math.BigDecimal;
+
 public class Account {
+
+
+    private Integer number;
+
+
     private Long id;
     private Integer accountNumber;
     private Integer agencyNumber;
     private Double balance;
+
+    public void deposit(BigDecimal amount)  {
+        this.balance.add(amount);
+    }
+    public void withdraw(BigDecimal amount) {
+        // Compare usando compareTo em vez de >= porque BigDecimal não suporta operadores diretamente
+        if (this.balance.compareTo(amount) >= 0) {
+            // É necessário atribuir o resultado de subtract de volta à balance, pois BigDecimal é imutável
+            this.balance = this.balance.subtract(amount);
+        } else {
+            // Complete a exceção que você deseja lançar
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+    }
+
+    public void transfer(Account origin, Account destiny, BigDecimal amount) {
+
+        if (origin.balance.compareTo(amount) >= 0) {
+
+            destiny.balance.add(amount);
+            origin.balance = origin.balance.subtract(amount);
+
+        } else {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+    }
+
 
 }
---
+++ src/main/java/com/br/acme_pay/BalanceToWithdrawException.java
@@ -0,0 +1,9 @@
+package br.com.acme_pay;
+
+public class BalanceToWithdrawException extends Exception{
+
+    public BalanceToWithdrawException(String message)  {
+        super();
+    }
+
+}
